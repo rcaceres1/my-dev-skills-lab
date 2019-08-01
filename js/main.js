e4698692a9927a4621f4d19bd632d027f67ac6ac
Ruben Caceres Jr <rcaceres.jr@gmail.com>
2019-07-31 21:30:06 -0500

    changes to newSkill click event

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 //someone writes a skill in the input
 var $newSkill;
 
-$('$newSkill').on('click', '.add-skill', function(evt) {
-    var template = `
-    <tr>
-        <td></td>
-        <td><button class="deleteButton">Delete</button></td>
-    </tr>
-    `;
-    $template.append('tbody').fadeIn(2000)
-});
+$('$newSkill').on('form', 'click'), function(evt) {
+    $('#table').childs('tr').append('<tr><td></td><td><button class="delete-button">Delete</button></td></tr>');
+};
 
 $('#skills tbody').on('click', '.delete-button', function() {
     $(this).closest('tr')
